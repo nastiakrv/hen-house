f8e9242f89dc113601afb9afcb61ea4b8f57d0a1
Anastasiia Kravets <nastya030915@gmail.com>
2024-12-11 01:40:41 +0200

    started to change filter.jsx 3:13

--- client/src/components/filter/filter.jsx
+++ client/src/components/filter/filter.jsx
@@ -1,22 +1,45 @@
+import { useState } from 'react';
+import { useSearchParams } from "react-router-dom";
 import './filter.scss'
 
 function Filter() {
-    return(
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState({
+        type: searchParams.get("type") || "",
+        city: searchParams.get("city") || "",
+        property: searchParams.get("property") || "",
+        minPrice: searchParams.get("minPrice") || "",
+        maxPrice: searchParams.get("maxPrice") || "",
+        bedroom: searchParams.get("bedroom") || "",
+    });
+
+    const handleChange = (e) => {
+        setQuery({
+            ...query,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleFilter = () => {
+        setSearchParams(query);
+    };
+
+    return (
         <div className='filter'>
             <h1>Search results for<b> London</b></h1>
             <div className="top">
                 <div className="item">
                     <label htmlfor="city">Location</label>
-                    <input 
-                    type="text" 
-                    id="city" 
-                    name="city" 
-                    placeholder='City Location'/>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        placeholder='City Location' />
                 </div>
             </div>
 
             <div className="bottom">
-            <div className="item">
+                <div className="item">
                     <label htmlfor="type">Type</label>
                     <select name="type" id="type">
                         <option value="">Any</option>
@@ -34,30 +57,30 @@ function Filter() {
                 </div>
                 <div className="item">
                     <label htmlfor="minPrice">Min Price</label>
-                    <input 
-                    type="number" 
-                    id="minPrice" 
-                    name="minPrice" 
-                    placeholder='Any'/>
+                    <input
+                        type="number"
+                        id="minPrice"
+                        name="minPrice"
+                        placeholder='Any' />
                 </div>
                 <div className="item">
                     <label htmlfor="maxPrice">Max Price</label>
-                    <input 
-                    type="number" 
-                    id="maxPrice" 
-                    name="maxPrice" 
-                    placeholder='Any'/>
+                    <input
+                        type="number"
+                        id="maxPrice"
+                        name="maxPrice"
+                        placeholder='Any' />
                 </div>
                 <div className="item">
                     <label htmlfor="bedroom">Bedroom</label>
-                    <input 
-                    type="text" 
-                    id="bedroom" 
-                    name="bedroom" 
-                    placeholder='Any'/>
+                    <input
+                        type="text"
+                        id="bedroom"
+                        name="bedroom"
+                        placeholder='Any' />
                 </div>
                 <button>
-                    <img src="/search.png" alt=""/>
+                    <img src="/search.png" alt="" />
                 </button>
             </div>
 
