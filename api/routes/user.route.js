231fa6625471cc81e9fe055528ec666d2bcb3440
Anastasiia Kravets <nastya030915@gmail.com>
2024-12-11 03:42:14 +0200

    images in profilepage are seen

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,10 +5,12 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id", verifyToken, getUser);
+//router.get("/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
+//router.get("/profilePosts", verifyToken, profilePosts);
+
 
 
 
